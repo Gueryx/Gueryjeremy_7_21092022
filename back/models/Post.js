44bf70683312cff5de429ad1b872127b826ee9cd
jeremy guery <j.guery226@gmail.com>
2022-09-29 15:57:06 +0200

    sup' name du schema de données

--- back/models/Post.js
+++ back/models/Post.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 // Création d'un schéma de données
 const postSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    name: { type: String, required: true },
     description: { type: String, required: true },
     imageUrl: { type: String, required: false },
     likes: { type: Number, defaut: 0 },
