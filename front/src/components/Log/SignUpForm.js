f6290cd7c07d4f019c1782504b4b08dcfed8e075
jeremy guery <j.guery226@gmail.com>
2022-10-03 15:42:02 +0200

    avancement front-end /profil

---
+++ front/src/components/Log/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <div>
+            S'enregister
+        </div>
+    );
+};
+
+export default SignUpForm;
--- front/src/pages/Profil.js
+++ front/src/pages/Profil.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Log from '../components/Log';
 
 const Profil = () => {
     return (
-        <div>
-            Depuis le profil
+        <div className="profil-page">
+            <div className="log-container">
+                <Log />
+                <div className="img-container">
+                    <img src="/images/icon-left-font-monochrome-black.png" alt="logo entreprise" />
+                </div>
+            </div>
         </div>
     );
 };
